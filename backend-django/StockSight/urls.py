07fafa5174ec0fe8a3d2d52dc7251b1dab8aa5d4
Ansh Mahajan <anshmahajan@Anshs-MacBook-Air-5.local>
2026-02-01 22:49:12 +0530

    deployment errors fixing

--- backend-django/StockSight/urls.py
+++ backend-django/StockSight/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "healthy", "message": "StockSight API is running"})
 
 urlpatterns = [
+    path("", health_check, name="health_check"),
     path("admin/", admin.site.urls),
 
     # Base API Endpoint
